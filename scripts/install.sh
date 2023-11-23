ab03c9ae8e041660bc389294e5ec068ecebab793
박광렬 <48284967+KwangryeolPark@users.noreply.github.com>
2023-11-23 16:30:44 +0900

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 pip install \
-    torch==2.1.1 \
-    torchvision==0.16.1 \
-    numpy==1.26.2 \
-    matplotlib==3.8.2 \
-    tqdm==4.66.1 \
     pytorch-lightning==2.1.2 \
     accelerate==0.24.1
