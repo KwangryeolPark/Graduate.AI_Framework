2a0982e4a657f014c490319de7d655f941c04589
KwangryeolPark <pkr7098@gmail.com>
2023-11-23 12:36:01 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Graduate.AI_Framework
 AI Framework Final Project.
+
+# How to install packages
+
+```
+bash scripts/install.sh
+```
---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install \
+    torch==2.1.1 \
+    torchvision==0.16.1 \
+    numpy==1.26.2 \
+    matplotlib==3.8.2 \
+    tqdm==4.66.1 \
+    pytorch-lightning==2.1.2
