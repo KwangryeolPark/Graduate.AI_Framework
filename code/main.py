16057a124dae22af3b8aad6e02b88b6555a52880
KwangryeolPark <pkr7098@gmail.com>
2023-11-23 10:53:04 +0000

    Modify typo

--- code/main.py
+++ code/main.py
@@ -10,11 +10,8 @@ from src.trainer import Trainer
 if __name__ == "__main__":
     args = args_parser()
     set_seed(args.seed)
-        
-    if args.data.upper() == 'CIFAR10':
-        num_classes = 10
-    elif args.data.upper() == 'CIFAR100':
-        num_classes = 100
+    
+    num_classes = 10 if args.data.upper() == 'CIFAR10' else 100
    
     if args.wandb == True:
         wandb.init(project = args.wandb_project_name, 
@@ -24,5 +21,5 @@ if __name__ == "__main__":
     trainer = Trainer(config)
     trainer.fit()
     
-    with open('./result,pkl', 'wb') as f:
+    with open('./result.pkl', 'wb') as f:
         pickle.dump(trainer.result, f)
