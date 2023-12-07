866eaa1244fc33e0fead8ecefd2d36e79adcfa28
KwangryeolPark <pkr7098@gmail.com>
2023-12-07 18:20:33 +0000

    update

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import os
-import pickle
-import json
-import random
-from src.accelerator import get_accelerator
-from src.trainer import Trainer
-from src.options import args_parser
+from utils.trainer import Trainer
+from utils.args import get_args
 
-if __name__ == "__main__":
-    args = args_parser()
-    accelerator = get_accelerator(args)
+def main():
+    args = get_args()
+    trainer = Trainer(args)
     
-    trainer = Trainer(args, accelerator)
     trainer.fit()
     
-    rand = random.randint(0, 10000)
-    
-    os.makedirs('./results/log', exist_ok=True)
-    with open(f'./results/log/{args.timestamp}_{rand}.pkl', 'wb') as f:
-        pickle.dump(trainer.result, f)
-        
-    os.makedirs('./results/configs', exist_ok=True)
-    with open(f'./results/configs/{args.timestamp}_{rand}.json', 'w') as f:
-        json.dump(vars(args), f, indent=4)
+if __name__ == '__main__':
+    main()
